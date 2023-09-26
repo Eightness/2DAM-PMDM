41b9dfbce042e46a099a7619c0ed2c836672db68
albert <albertlb08@gmail.com>
2023-09-26 12:45:20 +0200

    Last updateç

--- UD2-Fundamentos-de-Kotlin/Actividad 3/MajorMinorAverageAlbert/src/Main.kt
+++ UD2-Fundamentos-de-Kotlin/Actividad 3/MajorMinorAverageAlbert/src/Main.kt
@@ -13,8 +13,12 @@
 fun main() {
     //Inicializamos y declaramos variables útiles para el programa.
     var number: Int
-    var minor = Int.MAX_VALUE
+    /*
+    var minor = Int.MAX_VALUE       En un principio lo hice así, comprobando desde el primer momento.
     var major = Int.MIN_VALUE
+    */
+    var minor: Int? = null          //He decidido darles valor null para poder usarlo en las condiciones de asignación.
+    var major: Int? = null
     var sum = 0
     var index = 0
 
@@ -26,21 +30,24 @@ fun main() {
 
         //Condición para verificar que el usuario no ha introducido un 0.
         if (number != 0 ) {
-            index++         //Sumamos +1 al index para saber cuántos números se han introducido. (Nos servirá para calcular la media).
+            index++         //Sumamos +1 al index para saber cuántos 3números se han introducido. (Nos servirá para calcular la media).
             sum += number   //Sumamos el número introducido por el usuario.
 
             //Sobreescribimos las variables "minor" y "major" para actualizarlas, si consta.
-            if (number < minor) minor = number
-            if (number > major) major = number
+            if (minor == null || number < minor) minor = number
+            if (major == null || number > major) major = number
         }
     } while (number != 0)
 
-    //Calculamos la media.
-    val average: Double = sum.toDouble() / index.toDouble()
+    //Condición para comprobar si al menos se ha introducido un número.
+    if (index != 0) {
+        //Calculamos la media.
+        val average: Double = sum.toDouble() / index.toDouble()
 
-    //Resultado por pantalla.
-    println("""|El número más pequeño introducido es: $minor
+        //Resultado por pantalla.
+        println("""|El número más pequeño introducido es: $minor
                |El número más grande introducido es: $major
                |La media de los números introducidos es: $average
             """.trimMargin())
+    }
 }
