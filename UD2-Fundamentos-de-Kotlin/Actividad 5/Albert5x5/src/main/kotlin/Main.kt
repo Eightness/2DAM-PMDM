f8d0ad872feafbfc5dd68045fa2925c0afdd4a88
Albert <albertlb08@gmail.com>
2023-10-06 20:50:41 +0200

    matrix5x5

--- UD2-Fundamentos-de-Kotlin/Actividad 5/Albert5x5/src/main/kotlin/Main.kt
+++ UD2-Fundamentos-de-Kotlin/Actividad 5/Albert5x5/src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import java.lang.Exception
+import kotlin.random.Random
+import kotlin.system.exitProcess
+
 /**
  * Matrix5x5. El programa pedirá al usuario un número N entre 3 y 6. Con ese número creará una matriz de NxN.
  * A continuación, mostrará un menú y según la opción elegida realizará la acción indicada.
@@ -24,16 +28,62 @@
  * Función main. Ejecuta el programa.
  */
 fun main() {
+    println()
+    println("¡Bienvenido a la aplicación Matriz 5x5! por Albert Lozano.")
+    println()
+    val matrix = generateMatrix(getInt("Dime de qué tamaño quieres tu matriz NxN (Valor entre 3 y 6): ", 3, 6))
 
+    while (true)
+        options(menu(), matrix)
 }
 
 /**
  * Función getInt. Se encargará de recoger cualquier valor entero introducido por el usuario y devolverlo validado.
- * @return Devolverá un entero introducido por el usuario.
+ * @param message Le pasaremos una String con el mensaje que queramos mostrarle al usuario.
+ * @return Devolverá un entero introducido por el usuario validado.
  */
-fun getInt(): Int {
+fun getInt(message: String): Int {
+    //Estructura while para pedirle al usuario un número entero hasta que lo introduzca correctamente.
+    while (true) {
+        print(message)
+        try {
+            return readln().toInt()
+        } catch (e: NumberFormatException) {
+            print("No se ha introducido un número entero. ")
+        }
+    }
+}
 
-    return 0
+/**
+ * Función getInt. (Sobrecarga). Se encargará de recoger cualquier valor entero que esté entre un número x e y.
+ * @param message Mensaje para el usuario.
+ * @param minNumber Primer número.
+ * @param maxNumber Segundo número.
+ * @return Devolverá el número entero introducido el usuario validado.
+ */
+fun getInt(message: String, minNumber: Int, maxNumber: Int): Int {
+    //Estructura while para pedirle al usuario un número entero hasta que lo introduzca correctamente.
+    while (true) {
+        print(message)
+        try {
+            val number = readln().toInt()
+            if (number in minNumber..maxNumber)
+                return number
+            else throw Exception("El número tiene que estar entre $minNumber y $maxNumber. ")
+        } catch (e: NumberFormatException) {
+            print("No se ha introducido un número entero. ")
+        } catch (e: Exception) {
+            print("${e.message}")
+        }
+    }
+}
+
+/**
+ * Función generateMatrix. Se encargará de generar una matriz de N x N.
+ * @param number Será N.
+ */
+fun generateMatrix(number: Int): Array<Array<Int>> {
+    return Array(number) { Array(number) { 0 } }
 }
 
 /**
@@ -41,16 +91,36 @@ fun getInt(): Int {
  * @return Devolverá la opción elegida por el usuario.
  */
 fun menu(): Int {
-
-    return 0
+    println("""
+            |
+            |1.- Rellenar matriz (números aleatorios entre 0 y 9).
+            |2.- Mostrar matriz.
+            |3.- Buscar número.
+            |4.- Añadir uno. (Si la suma es 10, se convertirá a 0).
+            |5.- Traspuesta.
+            |0.- Salir.
+            |
+            """.trimMargin("|"))
+    return getInt("Elige una opción: ")
 }
 
 /**
  * Función options. Se encargará de manejar la estructura "when" para operar según la opción elegida por el usuario.
  * @param option Se le pasará como parámetro la opción elegida por el usuario.
  */
-fun options(option: Int) {
-
+fun options(option: Int, matrix: Array<Array<Int>>) {
+    when(option) {
+        1 -> fillMatrixRandom0To9(matrix, 0, 9)
+        2 -> showMatrix(matrix)
+        3 -> searchNumberInMatrix(matrix, getInt("¿Qué número quieres buscar? "))
+        4 -> addOneToEachPos(matrix)
+        5 -> transposeMatrix(matrix)
+        0 -> {
+            println("¡Muchas gracias por usar mi aplicación, hasta luego!")
+            exitProcess(0)
+        }
+        else -> println("Opción introducida inválida. Algo salió mal.")
+    }
 }
 
 /**
@@ -60,7 +130,10 @@ fun options(option: Int) {
  * @param maxNumber le pasaremos como parámetro un entero máximo.
  * @return Devolverá la matriz rellenada.
  */
-fun fillMatrixRandom(matrix: Array<Array<Int>>, minNumber: Int, maxNumber: Int): Array<Array<Int>> {
+fun fillMatrixRandom0To9(matrix: Array<Array<Int>>, minNumber: Int, maxNumber: Int): Array<Array<Int>> {
+    for ((i, _) in matrix)
+        for((j, _) in matrix)
+            matrix[i][j] = Random.nextInt(minNumber, maxNumber + 1)
 
     return matrix
 }
@@ -70,7 +143,12 @@ fun fillMatrixRandom(matrix: Array<Array<Int>>, minNumber: Int, maxNumber: Int):
  * @param matrix Matriz que se le pasará como parámetro para mostrarla.
  */
 fun showMatrix(matrix: Array<Array<Int>>) {
-
+    println("Matriz:")
+    for ((i, _) in matrix) {
+        for((j, _) in matrix)
+            print("[${matrix[i][j]}] ")
+        println()
+    }
 }
 
 /**
@@ -79,7 +157,17 @@ fun showMatrix(matrix: Array<Array<Int>>) {
  * @param number Entero que se le pasará para buscarlo.
  */
 fun searchNumberInMatrix(matrix: Array<Array<Int>>, number: Int) {
+    var found = false
 
+    for ((i, _) in matrix) {
+        for((j, _) in matrix)
+            if (matrix[i][j] == number) {
+                found = true
+                println("El número $number se ha encontrado en la posicion [$i][$j] ")
+            }
+    }
+
+    if (!found) println("No se han encontrado resultados.")
 }
 
 /**
@@ -87,7 +175,11 @@ fun searchNumberInMatrix(matrix: Array<Array<Int>>, number: Int) {
  * @param matrix Matriz que se le pasará para operar con ella.
  */
 fun addOneToEachPos(matrix: Array<Array<Int>>) {
+    for ((i, _) in matrix)
+        for ((j, _) in matrix)
+            matrix[i][j] += 1
 
+    showMatrix(matrix)
 }
 
 /**
@@ -95,5 +187,5 @@ fun addOneToEachPos(matrix: Array<Array<Int>>) {
  * @param matrix Matriz que se le pasará para realizar la transposición.
  */
 fun transposeMatrix(matrix: Array<Array<Int>>) {
-
+    println("Hello hehe.")
 }
